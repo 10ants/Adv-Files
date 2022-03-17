ca8b473480fe86d77a9f1912c265f7e3548d34d6
Jesse Tennant <75157270+BeepBwop@users.noreply.github.com>
2022-03-16 21:44:32 -0700

    Create Person.h

---
+++ Person.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <list>
+
+class Dog;
+class SerializedObject;
+
+class Person
+{
+public:
+	static const int SID = 2;
+    static Person* NewPerson();
+    static void DeleteAll();
+    static Person* CreateFromSO( SerializedObject* tSO );
+    static Person* FindPersonFromID( int tID );
+
+	Person* mBestFriend = nullptr;// When serialized, it is their object id (both are ints)
+	Dog* mBestDog = nullptr;
+	int mAge = 0;
+
+    SerializedObject* Serialize(); // Only we know how to smoosh ourselves.  Everyone knows what an SO is
+
+private:
+	int mObjectID = 0; // Which person object am I?
+	Person() {} // Can't make a person without going through our factory
+
+	static std::list<Person*> sAllPeople;
+};
+
