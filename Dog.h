4f44d33141a4acf8b9c06b84c732d26a0512e6db
Jesse Tennant <75157270+BeepBwop@users.noreply.github.com>
2022-03-23 15:05:30 -0700

    Update Dog.h

--- Dog.h
+++ Dog.h
@@ -1,30 +1,47 @@
-#pragma once
-#include <list>
-
-
-class SerializedObject;
-
-class Dog
+#include "Dog.h"
+#include "SerializedObject.h"
+std::list<Dog*>Dog::sAllDogs;
+ Dog* Dog::NewDog()
 {
-public:
-	static const int SID = 1;
-    static Dog* NewDog();
-    static void DeleteAll();
-    static Dog* CreateFromSO( SerializedObject* tSO );
-    static Dog* FindDogFromID( int tID );
+     Dog* newDog = new Dog;
+     Dog::sAllDogs.push_back(newDog);
+     newDog->mObjectID = Dog::sAllDogs.size();
+     return newDog;
+}
 
-	int mAge = 0;
+void Dog::DeleteAll()
+{
+    std::list<Dog*>::iterator iter;
+    for (auto iter = Dog::sAllDogs.begin(); iter != Dog::sAllDogs.end(); ++iter)
+        {
+           Dog::sAllDogs.erase(iter);
+       }
+}
 
-    SerializedObject* Serialize(); // Only we know how to smoosh ourselves.  Everyone knows what an SO is
+SerializedObject* Dog::Serialize()
+{
+    int mSize = sizeof(*this);
+    SerializedObject* SO = new SerializedObject;
+    SO->mClassID = SID;
+    SO->mFullSize = sizeof(*this);
+    SO->mBufferSize= (sizeof(int)*3);
+    SO->mBuffer = new char[mSize];
+    SO->mBuffer[0] = mAge & 0xff;
+    SO->mBuffer[1]= (mAge>>8) & 0xff;
+    SO->mBuffer[2] = (mAge>>16) & 0xff;
+    SO->mBuffer[3] = (mAge>>7) & 0xff;
+    SO->mBuffer[0] = (mObjectID) & 0xff;
+    SO->mBuffer[1] = (mObjectID>>8) & 0xff;
+    SO->mBuffer[2] = (mObjectID>>16) & 0xff;
+    SO->mBuffer[3] = (mObjectID>>7) & 0xff;
+    return SO;
+}
 
-private:
-	int mObjectID = 0; // Which Dog object am I?
-	Dog()
-    {
-        mObjectID =2;
-        mAge =24;
-    }// Can't make a person without going through our factory
-	static std::list<Dog*> sAllDogs;
+Dog* Dog::CreateFromSO( SerializedObject* tSO )
+{
+    return nullptr;
 
-};
+}
 
+
+}
