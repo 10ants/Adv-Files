cd73b35545ed516a12968017bcc15b35032e8600
Jesse Tennant <75157270+BeepBwop@users.noreply.github.com>
2022-03-23 15:06:35 -0700

    Update dog.cpp

--- dog.cpp
+++ dog.cpp
@@ -1,38 +1,41 @@
 #include "Dog.h"
 #include "SerializedObject.h"
-
+std::list<Dog*>Dog::sAllDogs;
  Dog* Dog::NewDog()
 {
      Dog* newDog = new Dog;
-     sAllDogs.push_back(newDog);
+     Dog::sAllDogs.push_back(newDog);
+     newDog->mObjectID = Dog::sAllDogs.size();
      return newDog;
 }
 
 void Dog::DeleteAll()
 {
     std::list<Dog*>::iterator iter;
-    for (auto iter = sAllDogs.begin(); iter != sAllDogs.end(); ++iter)
+    for (auto iter = Dog::sAllDogs.begin(); iter != Dog::sAllDogs.end(); ++iter)
         {
-           sAllDogs.erase(iter);
+           Dog::sAllDogs.erase(iter);
        }
 }
 
 SerializedObject* Dog::Serialize()
 {
+    int mSize = sizeof(*this);
     SerializedObject* SO = new SerializedObject;
     SO->mClassID = SID;
-    int mSize = sizeof(*this);
     SO->mFullSize = sizeof(*this);
-    SO->mBufferSize= mSize +3;
-
+    SO->mBufferSize= (sizeof(int)*3);
     SO->mBuffer = new char[mSize];
-    SO->mBuffer[0] =mAge<<6;
-    SO->mBuffer[0]= mAge<<3;
-    SO->mBuffer[0]=mAge<<7;
+    SO->mBuffer[0] = mAge & 0xff;
+    SO->mBuffer[1]= (mAge>>8) & 0xff;
+    SO->mBuffer[2] = (mAge>>16) & 0xff;
+    SO->mBuffer[3] = (mAge>>7) & 0xff;
+    SO->mBuffer[0] = (mObjectID) & 0xff;
+    SO->mBuffer[1] = (mObjectID>>8) & 0xff;
+    SO->mBuffer[2] = (mObjectID>>16) & 0xff;
+    SO->mBuffer[3] = (mObjectID>>7) & 0xff;
     return SO;
-    
 }
-
 
 Dog* Dog::CreateFromSO( SerializedObject* tSO )
 {
@@ -40,8 +43,3 @@ Dog* Dog::CreateFromSO( SerializedObject* tSO )
 
 }
 
-Dog* Dog::FindDogFromID( int tID )
-{
-    return nullptr;
-    
-}
