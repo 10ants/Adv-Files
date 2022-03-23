338a4ad3df68f649eb15728bacd852692cb8f051
Jesse Tennant <75157270+BeepBwop@users.noreply.github.com>
2022-03-23 15:06:03 -0700

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,46 +1,45 @@
 #include "Person.h"
 #include "SerializedObject.h"
+std::list<Person*>Person::sAllPeople;
 
 Person* Person::NewPerson()
 {
     Person* newPerson = new Person;
-    //sAllPeople.push_back(newPerson);
+    Person::sAllPeople.push_back(newPerson);
+    newPerson->mObjectID = Person::sAllPeople.size();
     return newPerson;
 }
 
 void Person::DeleteAll()
 {
     std::list<Person*>::iterator iter;
-    for (auto iter = sAllPeople.begin(); iter != sAllPeople.end(); ++iter)
+    for (auto iter = Person::sAllPeople.begin(); iter != Person::sAllPeople.end(); ++iter)
         {
-            sAllPeople.erase(iter);
+            Person::sAllPeople.erase(iter);
         }
 }
 
 SerializedObject* Person::Serialize()
 {
+    int mSize = sizeof(*this);
     SerializedObject* SO = new SerializedObject;
     SO->mClassID = SID;
-    int mSize = sizeof(*this);
     SO->mFullSize = sizeof(*this);
-    SO->mBufferSize= mSize +3;
-    
+    SO->mBufferSize= (sizeof(int)*3);
     SO->mBuffer = new char[mSize];
-    SO->mBuffer[0] = mAge<<29;
-    SO->mBuffer[0]= mAge<<19;
-    SO->mBuffer[0] = mAge<<10;
+    SO->mBuffer[0] = mAge & 0xff;
+    SO->mBuffer[1]= (mAge>>8) & 0xff;
+    SO->mBuffer[2] = (mAge>>16) & 0xff;
+    SO->mBuffer[3] = (mAge>>7) & 0xff;
+    SO->mBuffer[0] = (mObjectID) & 0xff;
+    SO->mBuffer[1] = (mObjectID>>8) & 0xff;
+    SO->mBuffer[2] = (mObjectID>>16) & 0xff;
+    SO->mBuffer[3] = (mObjectID>>7) & 0xff;
     return SO;
 }
-
-
 
 Person* Person::CreateFromSO( SerializedObject* tSO )
 {
     return nullptr;
 }
 
-Person* Person::FindPersonFromID( int tID )
-{
-    return nullptr;
-    
-}
